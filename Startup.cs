3b422c271c0f8c4f6b964190e819afa9db41b01e
Ahmad Jafari <71406620+sutedel@users.noreply.github.com>
2021-11-28 16:45:28 -0500

    Configure Database And create the models

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +33,12 @@ namespace CRUDspAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CRUDspAPI", Version = "v1" });
             });
+
+            services.AddDbContext<CRUDspAPI.Data.NorthwindContext>(
+                options =>
+                {
+                options.UseSqlServer(Configuration.GetConnectionString("NorthwindDB"));
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
